d8e79c5c5fff3643400378a6886bd42c85ab8bb7
Dakksel <113535342+Dakksel@users.noreply.github.com>
2026-08-18 02:59:41 +0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+/* =====================================================
+   НАСТРОЙКИ FIREBASE — Food House (филиал 2)
+   ===================================================== */
+const firebaseConfig = {
+  apiKey:            "AIzaSyBUIiKq03bNK2tHXlUCPNughGHyAmRo3ME",
+  authDomain:         "foodhouse13.firebaseapp.com",
+  projectId:          "foodhouse13",
+  storageBucket:      "foodhouse13.firebasestorage.app",
+  messagingSenderId:  "855198753261",
+  appId:              "1:855198753261:web:6e01bd9e1188b11b4647e4",
+};
+
+firebase.initializeApp(firebaseConfig);
